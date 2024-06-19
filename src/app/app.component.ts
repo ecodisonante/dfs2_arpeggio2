e51ccc670422adff8c70ebeaf7a58b58c562fdab
Francisco Valdés <ecodisonante@gmail.com>
2024-06-18 20:20:20 -0400

    base adaptada a componentes shared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
-  title = 'arpeggio2';
+  title = 'arpeggio5';
 }
